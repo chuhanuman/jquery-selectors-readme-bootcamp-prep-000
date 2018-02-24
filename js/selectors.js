0970833196c3018c9211766503a61873dddb330e
Hanuman Chu <chuhanuman@gmail.com>
2018-02-24 16:18:23 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,7 +5,7 @@ function lastImageSelector(){
   return $('img:last');
 }
 function ninjaBabySelector(){
-  return $('#babyninja');
+  return $('#baby-ninja');
 }
 function divSelector(){
   return $('.pics div');
