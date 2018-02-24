0c6eb46f8c96375b538654c40a21109074344070
Hanuman Chu <chuhanuman@gmail.com>
2018-02-24 16:11:30 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,14 +2,14 @@ function paragraphSelector(){
   return $('p');
 }
 function lastImageSelector(){
-  
+  return $('img:last');
 }
 function ninjaBabySelector(){
-  
+  return $('#babyninja')
 }
 function divSelector(){
-  
+  return $('div .pics');
 }
 function firstListItem(){
-  
+  return $(ul .pic-list);
 }
